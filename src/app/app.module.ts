8ea5471c6944a74ca825a70d86c2b03fdf61a363
Laia <laia.igelmo.a@gmail.com>
2021-08-30 00:30:36 +0200

    Change names of requested booking components to create the new ones of pending visits.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { VisitaPageComponent } from './components/visita-page/visita-page.component';
-import { IconsModule } from 'angular-bootstrap-md';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { BookingFormComponent } from './components/booking-form/booking-form.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {VisitaPageComponent} from './components/visita-page/visita-page.component';
+import {IconsModule, MDBBootstrapModule} from 'angular-bootstrap-md';
+import {CalendarComponent} from './components/calendar/calendar.component';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+import {BookingFormComponent} from './components/booking-form/booking-form.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ManagmentRequestsPageComponent } from './managementComponents/managment-requests-page/managment-requests-page.component';
-import {RouterModule, Routes} from '@angular/router';
+import {ManagmentRequestsPageComponent} from './managementComponents/managment-requests-page/managment-requests-page.component';
+import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from '../app-routing.module';
-import { ManagementHeaderComponent } from './managementComponents/management-header/management-header.component';
-import { ManagementFooterComponent } from './managementComponents/management-footer/management-footer.component';
-import {
-  BookingRequestListComponent
-} from './managementComponents/booking-request-list/booking-request-list.component';
+import {ManagementHeaderComponent} from './managementComponents/management-header/management-header.component';
+import {ManagementFooterComponent} from './managementComponents/management-footer/management-footer.component';
+import {BookingRequestListComponent} from './managementComponents/booking-request-list/booking-request-list.component';
 import {FormsModule} from '@angular/forms';
 import {BookingRequestItemComponent} from './managementComponents/booking-request-item/booking-request-item.component';
-import { VisitInfoComponent } from './managementComponents/visit-info/visit-info.component';
+import {RequestedBookingInfoComponent} from './managementComponents/requested-booking-info/requested-booking-info.component';
+import {ManagementVisitsPageComponent} from './managementComponents/management-visits-page/management-visits-page.component';
+import {VisitsListComponent} from './managementComponents/visits-list/visits-list.component';
+import {VisitItemComponent} from './managementComponents/visit-item/visit-item.component';
+import {VisitInfoComponent} from './managementComponents/visit-info/visit-info.component';
 
 @NgModule({
   declarations: [
@@ -39,6 +39,10 @@ import { VisitInfoComponent } from './managementComponents/visit-info/visit-info
     ManagementFooterComponent,
     BookingRequestListComponent,
     BookingRequestItemComponent,
+    RequestedBookingInfoComponent,
+    ManagementVisitsPageComponent,
+    VisitsListComponent,
+    VisitItemComponent,
     VisitInfoComponent,
   ],
   imports: [
